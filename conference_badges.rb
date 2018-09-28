0150f0efd9fc8d641bc37e21832f4509f65b6047
David Gilbert <dagilbert333@gmail.com>
2018-09-28 23:09:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(attendees)
   
   # attendees.each do |attendee|
   attendees.each_with_index{ |attendee, index|
-    
+    room_assignments = 
   }
    
    # room_assignments << "Hello, #{attendee}! You'll be assigned to room #{counter}!"
