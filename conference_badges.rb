2fefdf02617c33a908a7b7d06191e07ca45158f8
David Gilbert <dagilbert333@gmail.com>
2018-09-28 23:06:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,8 +20,7 @@ def assign_rooms(attendees)
  return room_assignments
 end
  
- def printer(attendees, names)
-   puts batch_badge_creator(names)
-   puts assign_rooms(attendees)
+ def printer()
+  
  end
  
